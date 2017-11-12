4871c24704071a1230bf4e284e265e2c107c9a06
Florian Kromer <thinwybk@mailbox.org>
2017-11-12 11:35:53 +0100

    add assert functions, rename assert function
    - add assert_parameter_not_exists, assert_parameter_has_value,
      assert_parameter_in_range
    - rename assert_parameter_not_existing to assert_parameter_exists
    (asserts expect expressions to be true)

--- clients/rosdbcpy/src/rosdbcpy/__init__.py
+++ clients/rosdbcpy/src/rosdbcpy/__init__.py
@@ -18,7 +18,9 @@
 import rospy
 
 class ParameterContractViolation(Exception):
-    """Basic exception for contract violations raised by parameters."""
+    """
+    Basic exception for contract violations raised by parameters.
+    """
     def __init__(self, name, msg=None):
         if msg is None:
             # default error message
@@ -28,24 +30,95 @@ class ParameterContractViolation(Exception):
         self.name = name
 
 class ParameterValueViolation(ParameterContractViolation):
-    """Exception for value contract violations raised by parameters."""
+    """
+    Exception for value contract violations raised by parameters.
+    """
     def __init__(self, name, value):
         super(ParameterValueViolation, self).__init__(
             name, msg="Parameter %s violated contract with value %s" % (name, value))
         self.value = value
 
-def assert_parameter_not_existing(name):
-    """Assert if the parameter is not existing."""
+def assert_parameter_exists(name):
+    """
+    Indicates a contract violation if the parameter is expected to exist but if
+    it does not exist.
+
+    Args:
+        name (string): Name of the parameter.
+
+    Raises:
+        ParameterContractViolation: Raised if parameter is not existing.
+    """
     if not rospy.has_param(name):
         raise ParameterContractViolation(name, "Parameter %s not existing" % (name))
 
-def assert_parameter_out_of_range(name, lower_bound, upper_bound):
+def assert_parameter_not_exists(name):
     """
-    Assert if a parameter is not within the valid range.
+    Indicates a contract violation if the parameter is expected to not exist but
+    if it does exist.
+
+    Args:
+        name (string): Name of the parameter.
+
+    Raises:
+        ParameterContractViolation: Raised if parameter is existing.
     """
     if rospy.has_param(name):
-        # get parameter values
-        if lower_bound > parameter > upper_bound:
+        raise ParameterContractViolation(name, "Parameter %s existing" % (name))
+
+def assert_parameter_has_value(name, value):
+    """
+    Indicates a contract violation if it is expected that the parameter has a
+    specific value but if it has not.
+
+    Args:
+        name (string): Name of the parameter.
+        value (depends on the parameter type): Value of the parameter.
+
+    Raises:
+        ParameterValueViolation: Raised if parameter value is not like expected.
+    """
+    if rospy.has_param(name):
+        observed_value = rospy.get_param(name)
+        if value != observed_value:
+            ParameterValueViolation(name, value)
+    else:
+        raise ParameterContractViolation(name, "Parameter %s not existing" % (name))
+
+def assert_parameter_in_range(name, lower_bound, upper_bound):
+    """
+    Indicates a contract violation if it is expected that a parameter value of
+    type 32-bit integers has a value within a defined range but if it has not.
+
+    Args:
+        name (string): Name of the parameter.
+
+    Raises:
+        ParameterValueViolation: Raised if parameter value is not in the range.
+        ParameterContractViolation: Raised if parameter does not exist.
+    """
+    if rospy.has_param(name):
+        value = rospy.get_param(name)
+        if lower_bound > value > upper_bound:
             raise ParameterValueViolation(name, value)
     else:
-        raise ParameterContractViolation("Parameter not existing.")
+        raise ParameterContractViolation(name, "Parameter %s not existing" % (name))
+
+def assert_parameter_out_range(name, lower_bound, upper_bound):
+    """
+    Indicates a contract violation if it is expected that a parameter value of
+    type 32-bit integers has a value outside a defined range but if it has not.
+
+    Args:
+        name (string): Name of the parameter.
+
+    Raises:
+        ParameterValueViolation: Raised if parameter value is not outside the range.
+        ParameterContractViolation: Raised if parameter does not exist.
+    """
+    if rospy.has_param(name):
+        value = rospy.get_param(name)
+        if lower_bound > value > upper_bound:
+            raise ParameterValueViolation(name, value)
+    else:
+        raise ParameterContractViolation(name, "Parameter %s not existing" % (name))
