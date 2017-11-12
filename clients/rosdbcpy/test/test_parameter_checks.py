3b7c8160439a16db0c72d7230ae57aca796d6be9
Florian Kromer <thinwybk@mailbox.org>
2017-11-12 17:41:00 +0100

    add parameter test setup, teardown and tests
    simple subprocess roscore setup and teardown
    
    tests for assert_parameter_exists() and assert_parameter_not_exists()

--- clients/rosdbcpy/test/test_parameter_checks.py
+++ clients/rosdbcpy/test/test_parameter_checks.py
@@ -20,14 +20,40 @@ import unittest
 
 import rosunit
 import rosdbcpy
+import rospy
+import subprocess
 
 class ParameterContractViolationExceptions(unittest.TestCase):
     """Tests for the parameter contract violation exceptions."""
 
+    def setUp(self):
+        """Test class level setup executed before every test. Start the roscore."""
+        self.roscore = subprocess.Popen("roscore")
+
+    def tearDown(self):
+        """Test class level teardown executed after every test. Stop the roscore."""
+        self.roscore.terminate()
+
     def test_non_existing_parameters_raises_exception(self):
-        # implicit setup: no roscore and no parameter existing
+        """Verifies if an exception is raised due to a parameter which is
+        expected to exist."""
         with self.assertRaises(rosdbcpy.ParameterContractViolation):
-            rosdbcpy.assert_parameter_not_existing("blub")
+            rosdbcpy.assert_parameter_exists("blub")
+
+    def test_existing_parameter_raises_exception(self):
+        """Verifies if an exception is raised due to a parameter which is not
+        expected to exist."""
+        # specific function level setup
+        irrelevant_value = 1
+        rospy.set_param('/blub', irrelevant_value)
+
+        try:
+            # execution
+            with self.assertRaises(rosdbcpy.ParameterContractViolation):
+                rosdbcpy.assert_parameter_not_exists("blub")
+        finally:
+            # specifc function level teardown
+            rospy.delete_param('/blub')
 
 if __name__ == '__main__':
     rosunit.unitrun('test_parameter_checks', 'test_non_existing_parameters_raises_exception', ParameterContractViolationExceptions, coverage_packages=['rosdbcpy'])
